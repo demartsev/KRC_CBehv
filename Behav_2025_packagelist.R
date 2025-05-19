bf7ca4892d4983a8733f859330748495653ecec8
masterofmolerats <94361957+masterofmolerats@users.noreply.github.com>
2025-05-19 15:45:57 +0200

    Add files via upload
    An R script to install and load the packages required for the course

---
+++ Behav_2025_packagelist.R
@@ -0,0 +1,18 @@
+# List of packages to install
+packages <- c(
+  "tidyverse", "lme4", "bioacoustics", "warbler", "tuner", "seewave", "Soundgen",
+  "TTR", "behavr", "ggetho", "zeitgebr", "sleepr", "scales", "chron", "sp", 
+  "raster", "move", "ggmap", "mapproj", "lattice", "RColorBrewer", "adehabitatHR", "ks"
+)
+
+# Function to install packages if not already installed
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+}
+
+# Install all packages
+sapply(packages, install_if_missing)
+
+lapply(packages, require, character.only = T)
