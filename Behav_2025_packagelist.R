227ab848d3f3c9c3e5f8347873bcb1a9c5aa597c
masterofmolerats <94361957+masterofmolerats@users.noreply.github.com>
2025-05-21 17:18:59 +0200

    Update Behav_2025_packagelist.R

--- Behav_2025_packagelist.R
+++ Behav_2025_packagelist.R
@@ -23,35 +23,12 @@ library(zeitgebr) ; library(lubridate) ; library(hms) ; library(sleepr) ; #...et
 #method 2: all at once
 lapply(packages, require, character.only = T)
 
-#if packages are not installed by students here's some code which will download the packages and dependencies
-#then save the package zip files to the working directory
-#run this before the course and transfer the folder to a USB and share with students to install
-#' Get package dependencies
-#'
-#' @param packs A string vector of package names
-#'
-#' @return A string vector with packs plus the names of any dependencies
-getDependencies <- function(packs){
-  dependencyNames <- unlist(
-    tools::package_dependencies(packages = packs, db = available.packages(), 
-                                which = c("Depends", "Imports"),
-                                recursive = TRUE))
-  packageNames <- union(packs, dependencyNames)
-  packageNames
-}
-# Calculate dependencies
-packages.dep <- getDependencies(packages)
+#!STOP!#
 
-# Download the packages to the working directory.
-# Package names and filenames are returned in a matrix.
-setwd("C:.../offline_packages/") #adjust as necessary
-pkgInfo <- download.packages(pkgs = packages, destdir = getwd(), type = "win.binary")
-# Save just the package file names (basename() strips off the full paths leaving just the filename)
-write.csv(file = "pkgFilenames.csv", basename(pkgInfo[, 2]), row.names = FALSE)
-
-#then have the students run this on their computers
+#only run the next part if the packages were not installed to your computer before the course
+#install packages from a USB drive
 # Set working directory to the location of the package files
-setwd("D:/my_usb/offline_packages/")
+setwd("D:/my_usb/offline_packages/") #adjust as necessary
 
 # Read the package filenames and install
 pkgFilenames <- read.csv("pkgFilenames.csv", stringsAsFactors = FALSE)[, 1]
